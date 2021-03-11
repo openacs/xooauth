32b599e2f8734b4b0d357fb3ef16de472b7592d7
Antonio Pisano <antonio@elettrotecnica.it>
2021-03-11 10:54:36 +0000

    Use naviserver api to parse URL variables

--- www/admin/lti-test.tcl
+++ www/admin/lti-test.tcl
@@ -138,8 +138,7 @@ user_id=me100}}
 
         ## Add potential GET parameters from the launch url to the signature parameters
         array set uri [uri::split $launch_url]
-        foreach pair [split $uri(query) &] {
-           lassign [split $pair =] key value
+        foreach {key value} [ns_set array [ns_parsequery $uri(query)]] {
            lappend signature_parameters [list [ns_urldecode $key] [ns_urldecode $value]]
         }
 
